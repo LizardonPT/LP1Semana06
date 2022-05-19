8777afd62e6548700a6128fe1e23a357108c6353
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-05-19 16:12:16 +0100

    Lista de inimigos

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -8,6 +8,29 @@ namespace MyGame
         {
             Enemy e1 = new Enemy("  heheh  ");
             Console.WriteLine(e1.GetName());
+            Enemy loki = new Enemy("Loki");
+            loki.SetName("     Grindalokki     ");
+            Console.WriteLine($"Name is {loki.GetName()} and HP is {loki.GetHealth()}");
+
+            Console.WriteLine("Insert number of enemies:");
+            Console.Write("> ");
+            int enemy_number = Convert.ToInt32(Console.ReadLine());
+
+            Enemy[] enemy_list = new Enemy[enemy_number];
+
+            for (int i = 0; i < enemy_number; i++)
+            {
+                Console.WriteLine($"Give a name to Enemy {i+1}:");
+                Console.Write("> ");
+                string name = Console.ReadLine();
+                Enemy enemy = new Enemy(name);
+                enemy_list[i] = enemy;
+            }
+
+            for (int i = 0; i < enemy_number; i++)
+            {
+                Console.WriteLine(enemy_list[i].GetName());
+            }
 
         }
     }
